1053e5a0fa9b3987fbaa8c038ef04c1d07cd34d7
TAKIGOKUL <gokulgkblueheart@gmail.com>
2025-12-20 22:58:30 +0530

    Fix profile image path for production build

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const personalDetails = {
     email: "gokul23gopakumar@gmail.com",
     phone: "(+91) 8129-7250-07",
     location: "Kerala, India",
-    profileImg: "/src/assets/profile.jpg",
+    profileImg: "/profile.jpg",
     socials: {
         github: "https://github.com/TAKIGOKUL", // Inferred URL structure
         linkedin: "https://linkedin.com/in/gokul0gk", // Inferred URL structure
